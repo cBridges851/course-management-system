72bdfaec076d6f99008ddcd8e13e3ce47fced96c
Christa Bridges <christa@pebblepad.co.uk>
2022-04-10 15:09:33 +0100

    user-classes Created User abstract class with properties specified in UML.

---
+++ CourseManagementSystem/src/main/java/com/example/coursemanagementsystem/Models/User.java
@@ -0,0 +1,12 @@
+package com.example.coursemanagementsystem.Models;
+
+import java.util.Calendar;
+
+public abstract class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Calendar dateOfBirth;
+}
