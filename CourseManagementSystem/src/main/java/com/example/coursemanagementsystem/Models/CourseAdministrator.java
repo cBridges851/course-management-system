86c3c8f3a46f12ddb98fcdc3515bc57ed3624692
Christa Bridges <christa@pebblepad.co.uk>
2022-04-10 16:04:58 +0100

    user-classes Ensured CourseAdministrator extended User, and created Instructor class.

--- CourseManagementSystem/src/main/java/com/example/coursemanagementsystem/Models/CourseAdministrator.java
+++ CourseManagementSystem/src/main/java/com/example/coursemanagementsystem/Models/CourseAdministrator.java
@@ -1,11 +1,14 @@
 package com.example.coursemanagementsystem.Models;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
-public class CourseAdministrator {
+public class CourseAdministrator extends User {
     private ArrayList<Course> courses;
 
-    public CourseAdministrator(ArrayList<Course> courses) {
+    public CourseAdministrator(String username, String password, String firstName, String middleName, String lastName, Calendar dateOfBirth,
+                               ArrayList<Course> courses) {
+        super(username, password, firstName, middleName, lastName,dateOfBirth);
         this.courses = courses;
     }
 
---
+++ CourseManagementSystem/src/main/java/com/example/coursemanagementsystem/Models/Instructor.java
@@ -0,0 +1,17 @@
+package com.example.coursemanagementsystem.Models;
+
+import java.util.Calendar;
+
+public class Instructor extends User {
+    private Modules[] modules;
+
+    public Instructor(String username, String password, String firstName, String middleName, String lastName,
+                      Calendar dateOfBirth, Modules[] modules) {
+        super(username, password, firstName, middleName, lastName, dateOfBirth);
+        this.modules = modules;
+    }
+
+    public Modules[] getModules() {
+        return modules;
+    }
+}
