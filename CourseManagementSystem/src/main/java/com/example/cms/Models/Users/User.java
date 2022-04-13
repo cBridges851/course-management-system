1abb8fe8bd2ede60daa32d9b12b20bea6595cb7a
Christa Bridges <christa@pebblepad.co.uk>
2022-04-13 10:55:51 +0100

    user-classes Added JSDoc to the User class.

--- CourseManagementSystem/src/main/java/com/example/cms/Models/Users/User.java
+++ CourseManagementSystem/src/main/java/com/example/cms/Models/Users/User.java
@@ -23,26 +23,44 @@ public abstract class User {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * @return the username of the user.
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * @return the password the user has.
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * @return the user's first name.
+     */
     public String getFirstName() {
         return this.firstName;
     }
 
+    /**
+     * @return the user's middle name.
+     */
     public String getMiddleName() {
         return this.middleName;
     }
 
+    /**
+     * @return the user's last name.
+     */
     public String getLastName() {
         return this.lastName;
     }
 
+    /**
+     * @return the user's birthday.
+     */
     public Calendar getDateOfBirth() {
         return this.dateOfBirth;
     }
