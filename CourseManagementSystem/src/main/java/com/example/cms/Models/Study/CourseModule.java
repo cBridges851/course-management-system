bc23a4c0f8df1113d5dc1fa9fa2a5b23cd5a6e35
Christa Bridges <christa@pebblepad.co.uk>
2022-04-13 11:26:23 +0100

    study Refactored CourseModule class.

--- CourseManagementSystem/src/main/java/com/example/cms/Models/Study/CourseModule.java
+++ CourseManagementSystem/src/main/java/com/example/cms/Models/Study/CourseModule.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Model that represents a module that could be on a course. For example, 5CS019 - Object-Oriented Programming
  */
 public class CourseModule {
-    private String moduleCode;
+    private String courseModuleCode;
     private String name;
     private int level;
     private Instructor instructor;
@@ -18,6 +18,7 @@ public class CourseModule {
 
     public CourseModule(String courseModuleCode, String name, int level, Instructor instructor, boolean isMandatory,
                         ArrayList<Assignment> assignments, ArrayList<StudentResult> studentResults) {
+        this.courseModuleCode = courseModuleCode;
         this.name = name;
         this.level = level;
         this.instructor = instructor;
@@ -30,28 +31,28 @@ public class CourseModule {
      * @return the code that identifies the module.
      */
     public String getCourseModuleCode() {
-        return moduleCode;
+        return this.courseModuleCode;
     }
 
     /**
      * @return the name of the course module.
      */
     public String getName() {
-        return name;
+        return this.name;
     }
 
     /**
      * @return the level of the course module. E.g. 4, 5 or 6.
      */
     public int getLevel() {
-        return level;
+        return this.level;
     }
 
     /**
      * @return the instructor who is teaching the module.
      */
     public Instructor getInstructor() {
-        return instructor;
+        return this.instructor;
     }
 
     /**
@@ -59,20 +60,20 @@ public class CourseModule {
      * false indicates that it is optional.
      */
     public boolean getIsMandatory() {
-        return isMandatory;
+        return this.isMandatory;
     }
 
     /**
      * @return the assignments that must be completed as part of the module.
      */
     public ArrayList<Assignment> getAssignments() {
-        return assignments;
+        return this.assignments;
     }
 
     /**
      * @return The results that students have got on the module.
      */
     public ArrayList<StudentResult> getStudentResults() {
-        return studentResults;
+        return this.studentResults;
     }
 }
